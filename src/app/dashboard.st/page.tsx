05fb272b8c1c1658ae9c9b8ae72b691aa59c71bd
Stephen Onu <stephenonu16@yahoo.com>
2024-08-14 13:48:22 +0100

    appointment list final

--- src/app/dashboard.st/page.tsx
+++ src/app/dashboard.st/page.tsx
@@ -1,8 +1,19 @@
-export default function Home() {
+import UpcomingAppointments from '@/app/ui.st/dashboard.st/UpcomingAppointments';
+
+export default function DashboardHome() {
     return (
-      <div>
-        <h1 className="text-4xl font-bold">Home</h1>
-        
-      </div>
-    );
-  }
+    <div className="flex h-full">
+        <div className="flex-grow p-6 md:p-12">
+            <h1 className="text-5xl font-bold mb-6">Home</h1>
+            
+            <div className="mb-8">
+              <p>Welcome to your dashboard.</p>
+            </div>
+        </div>        
+            <div className="w-full md:w-1/3 p-6 overflow-y-auto h-full bg-white shadow-lg">
+        <UpcomingAppointments />
+            </div>
+          </div>
+          
+      );
+    }
