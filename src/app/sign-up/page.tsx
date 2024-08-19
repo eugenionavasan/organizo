a5865b088822f1a3d56c535d23b7201bb2a24573
andre-amato <andreamatoprs@gmail.com>
2024-08-19 17:39:10 -0300

    fix: login clerck working

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -2,35 +2,17 @@
 
 import { SignUp } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
 
 const SignUpPage = () => {
-  const [errorMessage, setErrorMessage] = useState<string>('');
   const router = useRouter();
-
-  useEffect(() => {
-    const checkUser = async () => {
-      try {
-        const response = await fetch('/api/auth/check');
-        if (response.ok) {
-          router.push('/dashboard');
-        }
-      } catch (error) {
-        console.error('Error checking user status:', error);
-      }
-    };
-
-    checkUser();
-  }, [router]);
 
   return (
     <div className='flex flex-col justify-center items-center h-screen p-5 bg-gray-100'>
-      {errorMessage && <div className='text-red-600 mb-5'>{errorMessage}</div>}
       <SignUp
         path='/sign-up'
         routing='path'
         signInUrl='/login'
-        forceRedirectUrl='/verify-email-address'
+        forceRedirectUrl='/dashboard' // Changed from forceRedirectUrl
       />
     </div>
   );
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,13 @@
-import { NextResponse } from 'next/server';
-import { authMiddleware, clerkClient } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default authMiddleware({
-  publicRoutes: ['/', '/login'],
-  async afterAuth(auth, req, evt) {
-    // Handle routing for public routes
-    if (req.nextUrl.pathname === '/' || req.nextUrl.pathname === '/login') {
-      return;
-    }
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
 
-    // If the user is not signed in and the route is not public, redirect to login
-    if (!auth.userId) {
-      const loginUrl = new URL('/login', req.url);
-      return NextResponse.redirect(loginUrl);
-    }
-
-    // If the user is signed in and tries to access login, redirect to dashboard
-    if (auth.userId && req.nextUrl.pathname === '/login') {
-      const dashboardUrl = new URL('/dashboard', req.url);
-      return NextResponse.redirect(dashboardUrl);
-    }
-  },
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
-// Export the config
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
